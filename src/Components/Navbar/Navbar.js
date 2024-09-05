607253aaec0f9a49df44bb43ebe1e4ea675ccc1b
maxmulla23 <maxmulla23@gmail.com>
2024-09-05 14:31:14 +0300

    nav

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -18,13 +18,13 @@ const Navbar = () => {
       
       <ul
         tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li>
+        className="menu menu-sm dropdown-content bg-white text-black rounded-box z-[1] mt-3 w-52 p-2 shadow">
+        {/* <li>
           <a className="justify-between">
             Profile
             <span className="badge">New</span>
           </a>
-        </li>
+        </li> */}
         <li><a>Logout</a></li>
       </ul>
     </div>
