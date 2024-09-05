b61c0fa178bee8bf79bb66943b416bb8795e7fd4
maxmulla23 <maxmulla23@gmail.com>
2024-09-05 14:31:01 +0300

    get all journals

--- src/Components/Alljournals/Journal/Journal.js
+++ src/Components/Alljournals/Journal/Journal.js
@@ -1,31 +1,40 @@
-import React, { useState } from 'react'
-
-const journals = [
-    {
-        title: "hello world",
-        content: "i hope this will look beautiful",
-        
-    },
-    {
-        title: "collecting",
-        content: "we collect cookiies for absolutely no reason, we just like to annoy you bitch"
-    },
-    {
-      title: "Interface",
-      content: " I love how this interface looks nice"
-    }
-]
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
 
 const Journal = () => {
+  const [journals, setJournals] = useState([])
+  useEffect(() => {
+    const getJournals = async () => {
+      try {
+        const token = localStorage.getItem('token')
+        const response = await axios.get('http://localhost:5103/api/Journal', {
+          headers: {
+            Authorization: `Bearer ${token}`
+          },
+        })
+        setJournals(response.data)
+      } catch (error) {
+        toast.error("Couldn't get journals")
+        console.log(error);
+        
+      }
+    }
+    getJournals()
+  }, [])
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString)
+    return date.toISOString().split('T')[0]
+  }
   return (
     <div className='mr-3 space-y-3'>
       {journals.map((journal) => (
                 <div className="collapse collapse-arrow bg-gray-200 border">
-          
                 <input type="radio" name="my-accordion-2" defaultChecked />
-              
                 <div className="collapse-title text-lg text-blue-900 font-medium uppercase">{journal.title}</div>
                 <div className="collapse-content">
+                  <p className='text-pink-900'>{formatDate(journal.date)}</p>
                   <p className='text-gray-900'>{journal.content}</p>
                 </div>
               </div>
