8c18244ef184579f40d705d819428c56741c01e8
maxmulla23 <maxmulla23@gmail.com>
2024-09-17 15:16:46 +0300

    back

--- src/Components/Alljournals/Journal/Journal.js
+++ src/Components/Alljournals/Journal/Journal.js
@@ -5,6 +5,7 @@ import { toast } from 'react-toastify'
 const Journal = () => {
   const [journals, setJournals] = useState([])
   const [loading, setLoading] = useState(true)
+
   useEffect(() => {
     const getJournals = async () => {
       try {
@@ -18,32 +19,25 @@ const Journal = () => {
         setLoading(false)
       } catch (error) {
         toast.error("Couldn't get journals")
-        console.log(error);
+        console.log(error)
         setLoading(false)
       }
     }
     getJournals()
   }, [])
 
-
   return (
     <div className='mr-3 space-y-3 '>
-      {journals.map((journal) => (
-                <div role='alert' className="alert bg-gray-300 border-gray-300" key={journal.id}>
-                  {/* <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    className="h-6 w-6 shrink-0 stroke-current">
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                    </svg> */}
-                <span>
+      {loading ? (
+        <p>Loading journals...</p>  // Display loading message while journals are being fetched
+      ) : (
+        journals.length > 0 ? (
+          journals.map((journal) => (
+            <div role='alert' className="alert bg-gray-300 border-gray-300" key={journal.id}>
+              <span>
                 <p className="text-lg text-blue-900 font-medium uppercase">{journal.title}</p>
-                  <p className='text-gray-700'>{new Date(journal.date).toLocaleDateString(
+                <p className='text-gray-700'>
+                  {new Date(journal.date).toLocaleDateString(
                     "en-us",
                     {
                       weekday: "long",
@@ -51,11 +45,16 @@ const Journal = () => {
                       month: "long",
                       day: "numeric",
                     }
-                  )}</p>
-                  <p className='text-black'>{journal.content}</p>
-                </span>
-              </div>
-      ))}  
+                  )}
+                </p>
+                <p className='text-black'>{journal.content}</p>
+              </span>
+            </div>
+          ))
+        ) : (
+          <p className='font-serif text-3xl md:text-xl flex items-center justify-center'>You have no journal entries</p>  // Display this message if no journals are found
+        )
+      )}
     </div>
   )
 }
